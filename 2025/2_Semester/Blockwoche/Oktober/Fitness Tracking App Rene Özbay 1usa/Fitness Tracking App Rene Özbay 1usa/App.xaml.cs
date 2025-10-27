6ae4ff212fbe289600af1934c7e902a757a2b0b4
Tamao <reneozbay3@gmail.com>
2025-10-27 21:33:59 +0100

    push

---
+++ 2025/2_Semester/Blockwoche/Oktober/Fitness Tracking App Rene Özbay 1usa/Fitness Tracking App Rene Özbay 1usa/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Fitness_Tracking_App_Rene_Özbay_1usa
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ 2025/2_Semester/Blockwoche/Oktober/Fitness Tracking App Rene Özbay 1usa/Fitness Tracking App Rene Özbay 1usa/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace Fitness_Tracking_App_Rene_Özbay_1usa
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+            {
+                this.DragMove();
+            }
+        }
+
+        private void CloseApp_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
